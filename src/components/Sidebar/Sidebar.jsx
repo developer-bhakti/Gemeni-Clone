d8c249e885ce027955e934baf133dbd28014849d
purnale <bhaktipurnale89@gmail.com>
2025-04-09 10:42:27 +0530

    sidebar completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,39 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
-    <div className="h-[100vh] flex-col justify-between inline-flex bg-[#f0f4f9] p-[25px 15px]">
-      <div className="w-[120px]">
+    <div className="h-screen w-[250px] bg-[#f0f4f9] flex flex-col justify-between p-6">
+      <div>
         <img
-          className="inline-block ml-[10px] cursor-pointer"
+          onClick={() => setExtended((prev) => !prev)}
+          className="mb-6 cursor-pointer w-5 h-5"
           src={assets.menu_icon}
           alt="menu icon"
         />
-        <div className="mt-[10px] inline-flex items-center gap-[7px] p-[10px 15px] bg-[#e6eaf1] rounded-4xl text-[14px] text-gray-400 cursor-pointer">
-          <img src={assets.plus_icon} alt="plus icon" />
-          <p>New Chat</p>
+        <div className="flex items-center gap-2 p-3 bg-[#e6eaf1] rounded-2xl text-sm text-gray-500 cursor-pointer">
+          <img className="w-4 h-4" src={assets.plus_icon} alt="plus icon" />
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="flex flex-col">
-          <p className="mt-[20px] mb-[20px]">Recent</p>
-          <div className="flex items-start gap-[10px] p-[10px] pr-20 rounded-4xl text-[#282828] cursor-pointer">
-            <img src={assets.message_icon} alt="message icon" />
-            <p>What is React ...</p>
+        {extended ? (
+          <div className="mt-8">
+            <p className="text-gray-600 mb-4">Recent</p>
+            <div className="flex items-start gap-2 p-3 rounded-2xl text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+              <img
+                className="w-4 h-4"
+                src={assets.message_icon}
+                alt="message icon"
+              />
+              <p>What is React ...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
-      <div>
-        <div>
-          <img src={assets.question_icon} alt="question icon" />
-          <p>Help</p>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-700">
+        <div className="flex items-center gap-2 cursor-pointer hover:bg-[#e2e6eb] p-2 rounded-lg">
+          <img
+            className="w-4 h-4"
+            src={assets.question_icon}
+            alt="question icon"
+          />
+          {extended ? <p>Help</p> : null}
         </div>
-        <div>
-          <img src={assets.history_icon} alt="question icon" />
-          <p>Activity</p>
+        <div className="flex items-center gap-2 cursor-pointer hover:bg-[#e2e6eb] p-2 rounded-lg">
+          <img
+            className="w-4 h-4"
+            src={assets.history_icon}
+            alt="history icon"
+          />
+          {extended ? <p>Activity</p> : null}
         </div>
-        <div>
-          <img src={assets.setting_icon} alt="question icon" />
-          <p>Settings</p>
+        <div className="flex items-center gap-2 cursor-pointer hover:bg-[#e2e6eb] p-2 rounded-lg">
+          <img
+            className="w-4 h-4"
+            src={assets.setting_icon}
+            alt="setting icon"
+          />
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
